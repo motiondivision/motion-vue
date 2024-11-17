22487c08ab21ebfb62460ea3067ecf244637c650
Persephone Flores <34418758+hp0844182@users.noreply.github.com>
2024-11-17 18:02:52 +0800

    chore: trigger build

--- packages/motion/src/index.ts
+++ packages/motion/src/index.ts
@@ -1,3 +1,3 @@
 export * from 'framer-motion/dom'
-export { default as Motion } from './components/Motion.vue'
-export { default as AnimatePresence } from './components/AnimatePresence.vue'
+export { default as Motion, type MotionProps } from './components/Motion.vue'
+export { default as AnimatePresence, type AnimatePresenceProps } from './components/AnimatePresence.vue'
