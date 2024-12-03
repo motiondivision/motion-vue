1b33af400df6e9f54022daac4aa77f96b093975e
Persephone Flores <34418758+hp0844182@users.noreply.github.com>
2024-12-03 22:39:27 +0800

    feat: 添加自定义变体动画示例

---
+++ docs/src/lib/registry/new-york/example/CustomVariant.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Motion } from 'motion-v'
+import type { Variant } from 'motion-v'
+
+const variants: Record<string, Variant | ((custom: number) => Variant)> = {
+  invisible: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: custom => ({
+    opacity: 1,
+    y: 0,
+    transition: { delay: custom * 0.3, ease: 'easeInOut', duration: 0.4 },
+  }),
+}
+</script>
+
+<template>
+  <Motion
+    as="ul"
+    initial="invisible"
+    animate="visible"
+    class="space-y-4 w-1/2"
+  >
+    <Motion
+      v-for="i in 3"
+      :key="i"
+      as="li"
+      :custom="i - 1"
+      :variants="variants"
+      class="h-10 bg-primary rounded-lg "
+    />
+  </Motion>
+</template>
