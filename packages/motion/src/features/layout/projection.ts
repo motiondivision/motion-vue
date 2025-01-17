3e77130666000dd02a6b9821ee97b4464751b55e
Persephone Flores <34418758+hp0844182@users.noreply.github.com>
2025-01-17 21:54:13 +0800

    feat: add useDragControls export and refactor ProjectionFeature for improved layout handling

--- packages/motion/src/features/layout/projection.ts
+++ packages/motion/src/features/layout/projection.ts
@@ -3,7 +3,7 @@ import { HTMLProjectionNode } from 'framer-motion/dist/es/projection/node/HTMLPr
 import { getClosestProjectingNode } from '@/features/layout/utils'
 import { addScaleCorrector } from 'framer-motion/dist/es/projection/styles/scale-correction.mjs'
 import { defaultScaleCorrector } from '@/features/layout/config'
-import { doneCallbacks } from '@/components/presence'
+import { isHTMLElement } from '@/features/gestures/drag/utils/is'
 
 export class ProjectionFeature extends Feature {
   constructor(state) {
@@ -21,44 +21,19 @@ export class ProjectionFeature extends Feature {
         : getClosestProjectingNode(this.state.visualElement.parent),
     )
     this.state.visualElement.projection.isPresent = true
-    this.state.visualElement.projection.setOptions({
-      layout: options.layout,
-      layoutId: options.layoutId,
-      // TODO: drag
-      alwaysMeasureLayout: false,
-      visualElement: this.state.visualElement,
-      animationType: typeof options.layout === 'string' ? options.layout : 'both',
-      // initialPromotionConfig
-      layoutRoot: options.layoutRoot,
-      layoutScroll: options.layoutScroll,
-      crossfade: options.crossfade,
-      onExitComplete: () => {
-        if (!this.state.visualElement.projection?.isPresent) {
-          const done = doneCallbacks.get(this.state.element)
-          this.state.isSafeToRemove = true
-          if (done) {
-            done({
-              detail: {
-                isExit: true,
-              },
-            }, true)
-          }
-        }
-      },
-    })
+    this.setOptions()
   }
 
   beforeMount() {
     this.initProjection()
   }
 
-  update() {
+  setOptions() {
     const options = this.state.options
     this.state.visualElement.projection.setOptions({
       layout: options.layout,
       layoutId: options.layoutId,
-      // TODO: drag
-      alwaysMeasureLayout: false,
+      alwaysMeasureLayout: Boolean(options.drag) || (options.dragConstraints && isHTMLElement(options.dragConstraints)),
       visualElement: this.state.visualElement,
       animationType: typeof options.layout === 'string' ? options.layout : 'both',
       // initialPromotionConfig
@@ -66,6 +41,10 @@ export class ProjectionFeature extends Feature {
       layoutScroll: options.layoutScroll,
       crossfade: options.crossfade,
     })
+  }
+
+  update() {
+    this.setOptions()
   }
 
   mount() {
