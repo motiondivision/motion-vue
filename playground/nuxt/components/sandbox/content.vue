77f1ea4ffb51f79c360f1ba1d40adbca09d09f9f
ketsebaotgizachew <khubofficial00@gmail.com>
2025-01-29 19:05:23 +0300

    feat(sandbox): a new playground enviroment sandbox for testing things out added

---
+++ playground/nuxt/components/sandbox/content.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Motion } from 'motion-v'
+
+const phrases: Ref<string[]> = ref(['you', 'can', 'modify', 'component', '/sandbox', 'to', 'play', 'with', 'motion-v'])
+const phraseIndex = ref(0)
+
+function nextPhrase() {
+  phraseIndex.value = (phraseIndex.value + 1) % phrases.value.length
+}
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 place-items-center">
+    <span>Just A Simple Animation ! 😉</span>
+    <Motion
+      :animate="{
+        backgroundColor: '#ffffff33',
+        scale: 1,
+        filter: 'blur(0px)',
+      }"
+      :press="{
+        scale: 0.8,
+        filter: 'blur(15px)',
+        backgroundColor: '#ffffff66',
+        animationTimingFunction: 0.3,
+      }"
+      class="w-64 h-64 rounded-xl gradient-pink-violet backdrop-blur-sm flex items-center justify-center cursor-pointer"
+      @press="nextPhrase"
+      @press-cancel="console.log('press-cancel')"
+      @press-start="console.log('press-start')"
+    >
+      <span class="text-white select-none  text-xl font-bold">{{ phrases[phraseIndex] }}</span>
+    </Motion>
+  </div>
+</template>
--- playground/nuxt/pages/index.vue
+++ playground/nuxt/pages/index.vue
@@ -21,28 +21,51 @@ const routes = [
   { path: '/animated-tooltip', name: 'Animated Tooltip' },
 ]
 const isShow = ref(false)
+// toggle for sandbox mode
+const sandboxMode = ref(false)
 </script>
 
 <template>
-  <div class="p-8 h-screen overflow-auto bg-gradient-to-tr from-[#7b2ff7] to-[#f107a3]">
-    <h1 class="text-2xl flex max-sm:flex-col gap-2 place-items-center border-b border-white py-3 font-bold text-white">
-      Motion-V 动画演示
-      <button class="text-sm px-3 max-sm:ml-0 rounded-md shadow-md py-1 ml-auto bg-white hover:bg-gray-200 transition-all duration-300 text-black">
-        Open Sandbox
-      </button>
-    </h1>
-    <h2 class="text-lg font-semibold max-sm:text-center text-gray-200 py-3">
-      These are some examples of Motion-V animations.
-    </h2>
-    <div class="flex flex-wrap gap-3 max-sm:justify-center">
-      <NuxtLink
-        v-for="route in routes"
-        :key="route.path"
-        :to="route.path"
-        class="w-[250px] p-4 py-8 flex place-items-center justify-center bg-white/20 backdrop-blur-sm rounded-lg text-white hover:bg-white/30 transition-colors"
+  <div class="p-8 text-white py-2 flex flex-col gap-3 h-screen gradient-pink-violet">
+    <div class="text-2xl flex max-sm:flex-col gap-2 place-items-center border-b border-white pb-3 font-bold ">
+      <img
+        src="/logo.svg"
+        class="w-10"
+        alt=""
       >
-        {{ route.name }}
-      </NuxtLink>
+      Motion-V 动画演示
+      <span class="border-l text-sm border-white px-3">
+        playground
+      </span>
+      <button
+        class="text-sm px-4 max-sm:ml-0 rounded-md shadow-md py-2 ml-auto bg-white hover:bg-gray-200 transition-all duration-200 text-black"
+        @click="sandboxMode = !sandboxMode"
+      >
+        Go To Sandbox
+      </button>
+    </div>
+    <!-- Example Demos List -->
+    <div
+      v-if="!sandboxMode"
+      class="demos"
+    >
+      <h2 class="text-lg font-semibold max-sm:text-center text-gray-200 py-3">
+        These are some examples of Motion-V animations.
+      </h2>
+      <div class="flex flex-wrap gap-3 max-sm:justify-center">
+        <NuxtLink
+          v-for="route in routes"
+          :key="route.path"
+          :to="route.path"
+          class="w-[240px] p-4 py-8 flex place-items-center justify-center bg-white/20 backdrop-blur-sm rounded-lg text-white hover:bg-white/30 transition-colors"
+        >
+          {{ route.name }}
+        </NuxtLink>
+      </div>
+    </div>
+    <!-- Sandbox View -->
+    <div class="flex-1">
+      <Sandbox v-if="sandboxMode" />
     </div>
   </div>
 </template>
