1ca64e4ea7906e4f4f23918c1d9cc077c2a6fee4
Dennis Smuda <smuda.dennis@gmail.com>
2025-02-24 19:34:53 +0100

    docs: init use spring docs

---
+++ docs/components/demo/use-spring/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Motion, delay } from 'motion-v'
+
+const spring = { damping: 3, stiffness: 50, restDelta: 0.001 }
+const scaleValue = useMotionValue(0)
+const scale = useSpring(scaleValue, spring)
+
+delay(() => {
+  scale.set(1)
+}, 0.5)
+</script>
+
+<template>
+  <Motion
+    as="div"
+    :style="{
+      scale,
+    }"
+    class="size-20 bg-primary rounded-full"
+  />
+</template>
