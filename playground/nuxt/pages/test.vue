7cb854a691fc9ad34f26de83592f51b994eb4150
Persephone Flores <34418758+hp0844182@users.noreply.github.com>
2025-02-26 16:41:34 +0800

    feat: add interactive number formatting demo with compact and currency switches

--- playground/nuxt/pages/test.vue
+++ playground/nuxt/pages/test.vue
@@ -1,33 +1,130 @@
-<script setup lang="ts">
+<script setup lang="tsx">
+/** @jsxImportSource vue */
+import { AnimateNumber } from 'motion-number-vue'
+import { motion } from 'motion-v'
 import { ref } from 'vue'
-import { Motion, useMotionValue } from 'motion-v'
 
-const counter = ref(0)
-function updateComponent() {
-  counter.value += 1
+const isCompact = ref(true)
+const isCurrency = ref(false)
+
+function Switch({ isOn, toggle }: { isOn: boolean, toggle: () => void }) {
+  return (
+    <motion.button
+      class="switch-container"
+      style={{
+        justifyContent: `flex-${isOn ? 'end' : 'start'}`,
+      }}
+      initial={false}
+      animate={{
+        backgroundColor: isOn
+          ? 'var(--hue-6-transparent)'
+          : '#586d8c33',
+      }}
+      onClick={toggle}
+      focus={{
+        outline: '2px solid #4ff0b7',
+      }}
+    >
+      <motion.div
+        class="switch-handle"
+        layout
+        data-is-on={isOn}
+        transition={{
+          type: 'spring',
+          visualDuration: 0.2,
+          bounce: 0.2,
+        }}
+      />
+    </motion.button>
+  )
 }
-
-const rotate = useMotionValue(0)
-function updateMotionValue() {
-  rotate.set(rotate.get() + 10)
+const value = ref(5385)
+function changeCurrency() {
+  // value.value = 58
+  value.value = Math.random() * 1000 * (Math.random() > 0.5 ? 1 : -1) * 10 ** Math.floor(Math.random() * 3)
 }
 </script>
 
 <template>
-  <Motion
-    as-child
-
-    :style="{ rotate }"
+  <div
+    class="container"
+    @click="changeCurrency"
   >
-    <div class="w-10 h-10 bg-red-500">
-      {{ counter }}
+    <AnimateNumber
+      :format="{
+        notation: isCompact ? 'compact' : undefined,
+        compactDisplay: isCompact ? 'short' : undefined,
+        roundingMode: isCompact ? 'trunc' : undefined,
+        style: isCurrency ? 'currency' : undefined,
+        currency: isCurrency ? 'USD' : undefined,
+      }"
+      locales="en-US"
+      class="number"
+      :transition="{
+        visualDuration: 0.6,
+        type: 'spring',
+        bounce: 0.25,
+        opacity: { duration: 0.3, ease: 'linear' },
+      }"
+      :value="value"
+    />
+    <div class="controls">
+      <div>
+        Currency:
+        <Switch
+          :is-on="isCurrency"
+          :toggle="() => isCurrency = !isCurrency"
+        />
+      </div>
+      <div>
+        Compact:
+        <Switch
+          :is-on="isCompact"
+          :toggle="() => isCompact = !isCompact"
+        />
+      </div>
     </div>
-  </Motion>
-
-  <button @click="updateComponent">
-    Update component
-  </button>
-  <button @click="updateMotionValue">
-    Update motion value
-  </button>
+  </div>
 </template>
+
+<style>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+}
+
+.number {
+    font-size: 78px;
+}
+
+.controls {
+    display: flex;
+    gap: 20px;
+    border-radius: 50px;
+}
+
+.controls > div {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-size: 18px;
+}
+
+.switch-container {
+    width: 40px;
+    height: 20px;
+    border-radius: 25px;
+    cursor: pointer;
+    display: flex;
+    padding: 5px;
+}
+
+.switch-handle {
+    width: 20px;
+    height: 20px;
+    background-color: #4ff0b7;
+    border-radius: 50%;
+}
+</style>
