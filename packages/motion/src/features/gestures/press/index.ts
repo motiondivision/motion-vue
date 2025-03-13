8f2c63ebe3c19218f6ceb3abacaf9d2926a9cf42
Persephone Flores <34418758+hp0844182@users.noreply.github.com>
2025-03-13 22:56:26 +0800

    fix: every motion.div gets tabindex="0"

--- packages/motion/src/features/gestures/press/index.ts
+++ packages/motion/src/features/gestures/press/index.ts
@@ -2,6 +2,7 @@ import type { MotionState } from '@/state/motion-state'
 import { Feature } from '@/features'
 import { frame, press } from 'framer-motion/dom'
 import type { EventInfo } from 'framer-motion'
+import type { Options } from '@/types'
 
 export function extractEventInfo(event: PointerEvent): EventInfo {
   return {
@@ -35,7 +36,8 @@ function handlePressEvent(
 
 export class PressGesture extends Feature {
   isActive() {
-    return Boolean(this.state.options.press)
+    console.log('isActive', this.state.options.whilePress)
+    return Boolean(this.state.options.whilePress)
   }
 
   constructor(state: MotionState) {
@@ -43,22 +45,38 @@ export class PressGesture extends Feature {
   }
 
   mount() {
-    const element = this.state.element
-    if (!element)
-      return
-    this.unmount = press(
-      element,
-      (el, startEvent) => {
-        handlePressEvent(this.state, startEvent, 'Start')
+    this.register()
+  }
 
-        return (endEvent, { success }) =>
-          handlePressEvent(
-            this.state,
-            endEvent,
-            success ? 'End' : 'Cancel',
-          )
-      },
-      { useGlobalTarget: this.state.options.globalPressTarget },
-    )
+  update() {
+    const preProps = this.state.visualElement.prevProps as unknown as Options
+    // Re-register if whilePress changes
+    if (preProps.whilePress !== this.state.options.whilePress) {
+      this.register()
+    }
+  }
+
+  register() {
+    // Unmount previous press handler
+    this.unmount()
+    if (this.isActive()) {
+      const element = this.state.element
+      if (!element)
+        return
+      this.unmount = press(
+        element,
+        (el, startEvent) => {
+          handlePressEvent(this.state, startEvent, 'Start')
+
+          return (endEvent, { success }) =>
+            handlePressEvent(
+              this.state,
+              endEvent,
+              success ? 'End' : 'Cancel',
+            )
+        },
+        { useGlobalTarget: this.state.options.globalPressTarget },
+      )
+    }
   }
 }
