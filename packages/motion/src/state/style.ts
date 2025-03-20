cd6c7bc3822ff156ae48793bb605976078170fb5
Persephone Flores <34418758+hp0844182@users.noreply.github.com>
2025-03-20 23:19:54 +0800

    fix: initial pathLength value

--- packages/motion/src/state/style.ts
+++ packages/motion/src/state/style.ts
@@ -1,8 +1,9 @@
-import type { DOMKeyframesDefinition } from 'framer-motion'
+import type { DOMKeyframesDefinition, ResolvedValues } from 'framer-motion'
 import { isCssVar, isNumber } from './utils'
 import { buildTransformTemplate, isTransform, transformAlias, transformDefinitions } from './transform'
 import { isMotionValue } from '@/utils'
 import type { MotionStyle } from '@/types'
+import { px } from '@/value/types/numbers/units'
 
 type MotionStyleKey = Exclude<
   keyof CSSStyleDeclaration,
@@ -113,22 +114,40 @@ const SVG_STYLE_TO_ATTRIBUTES = {
   vectorEffect: true,
 } as const
 
+function buildSVGPath(
+  attrs: ResolvedValues,
+  length: number,
+  spacing = 1,
+  offset = 0,
+) {
+  attrs.pathLength = 1
+  // Build the dash offset
+  attrs['stroke-dashoffset'] = px.transform(-offset)
+
+  // Build the dash array
+  const pathLength = px.transform!(length)
+  const pathSpacing = px.transform!(spacing)
+  attrs['stroke-dasharray'] = `${pathLength} ${pathSpacing}`
+}
 export function convertSvgStyleToAttributes(keyframes?: MotionStyle | DOMKeyframesDefinition) {
   const attributes: Record<string, any> = {}
   const styleProps: Record<string, any> = {}
-
+  let shouldBuildPath = false
   for (const key in keyframes as any) {
     if (key in SVG_STYLE_TO_ATTRIBUTES) {
-      const attrKey = SVG_STYLE_TO_ATTRIBUTES[key as keyof typeof SVG_STYLE_TO_ATTRIBUTES]
-      const attrName = typeof attrKey === 'string' ? attrKey : key
+      if (key === 'pathLength') {
+        shouldBuildPath = true
+      }
       const value = keyframes[key]
-      attributes[attrName] = isMotionValue(value) ? value.get() : value
+      attributes[key] = isMotionValue(value) ? value.get() : value
     }
     else {
       styleProps[key] = keyframes[key]
     }
   }
-
+  if (shouldBuildPath) {
+    buildSVGPath(attributes, attributes.pathLength, attributes.pathSpacing, attributes.pathOffset)
+  }
   return {
     attributes,
     style: styleProps,
