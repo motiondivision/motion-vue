7c627d6595a891a96c6f1a0cdd3327caa272a091
Persephone Flores <34418758+hp0844182@users.noreply.github.com>
2026-02-15 20:33:00 +0800

    chore: fix type error

--- packages/motion/src/types/instance.ts
+++ packages/motion/src/types/instance.ts
@@ -1,5 +1,6 @@
 import type { Options } from '@/types/state'
 
+// @ts-expect-error
 declare module '@vue/runtime-dom' {
   interface HTMLAttributes extends Omit<Options, 'motionConfig' | 'layoutGroup'> {
   }
